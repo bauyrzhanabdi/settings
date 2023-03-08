59ec8d23afbc9824260fbaf480987d028d955bce
Bauyrzhan Abdi <bauyrzhan.abdi@nu.edu.kz>
2023-03-08 16:47:14 +0600

    added table view

--- Settings/Source/ViewController/SettingsViewController.swift
+++ Settings/Source/ViewController/SettingsViewController.swift
@@ -1,18 +1,61 @@
 import UIKit
+import SnapKit
 
 final class SettingsViewController: UIViewController {
 
     // MARK: - Outlets
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupView()
+        setupHierarchy()
+        setupLayout()
     }
     
     // MARK: - Setup
+    
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+        title = "Настройки"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupLayout() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
 
+// MARK: - Extension
 
+extension SettingsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.identifier, for: indexPath)
+        return cell
+    }
+}
+
+extension SettingsViewController: UITableViewDelegate {
+    
 }
 
