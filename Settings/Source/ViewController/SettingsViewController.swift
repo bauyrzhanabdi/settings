c733f754809d52244cc2eacdd8c1315923998cc3
Bauyrzhan Abdi <bauyrzhan.abdi@nu.edu.kz>
2023-03-08 16:28:13 +0600

    initial commit

---
+++ Settings/Source/ViewController/SettingsViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Setup
+
+
+}
+
